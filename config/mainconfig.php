ea1547703f7eb9f5e8738c09e10913326811d4a7
Fakhri Adi Saputra <fakhriadisaputra15@gmail.com>
2021-08-13 20:45:06 +0700

    Push Fix

--- config/mainconfig.php
+++ config/mainconfig.php
@@ -1,16 +1,3 @@
 <?php
 
 $cfg_baseurl = "/tugas_atol";
-
-$db_host = "localhost";
-$db_user = "root";
-$db_pass = "root";
-$db_name = "tubes_basdat";
-
-try {    
-    //create PDO connection 
-    $db = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
-} catch(PDOException $e) {
-    //show error
-    die("Terjadi masalah: " . $e->getMessage());
-}
